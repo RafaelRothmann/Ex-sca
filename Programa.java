029888356a907cba4b1da3d5dbbe523918db0a37
rafael.rothmann <r.rothmann@edu.pucrs.br>
2025-07-31 22:38:36 -0300

    Inicio

---
+++ Programa.java
@@ -0,0 +1,9 @@
+import obj.Carro;
+
+public class Programa {
+    public static void main(String[] args) {
+        Carro carro = new Carro("Toyota", "Corolla", "ABC-1234");
+
+        System.out.println("\n"+carro+"\n");
+    }
+}
---
+++ obj/Carro.java
@@ -0,0 +1,24 @@
+package obj;
+public class Carro {
+    public String marca;
+    public String modelo;
+    public String placa;
+
+    public Carro(String marca, String modelo, String placa) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.placa = placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String toString() {
+        return "Carro{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", placa='" + placa + '\'' +
+                '}';
+    }
+}
